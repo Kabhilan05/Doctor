ae670604cb0de3998671eaa1b3e85607f0b2da0f
Kabhilan05 <kabhilan05@gmail.com>
2023-10-25 23:40:51 +0530

    Login page created

--- doctor/client/src/pages/Login/index.js
+++ doctor/client/src/pages/Login/index.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { Divider, Form } from "antd";
+import { Divider, Form,Button, } from "antd";
+import {GoogleCircleFilled }from "@ant-design/icons"
+import{Link} from "react-router-dom"
 import bgimg from "../../images/Loginbg2.jpg";
-import Button from "../../components/Button";
+
+
 const Login = () => {
   return (
     <div
@@ -11,27 +14,55 @@ const Login = () => {
       <div className="  flex bg-slate-400/40 w-2/6 justify-center shadow-2xl shadow-slate-200 rounded-md  ">
         <Form
           layout="vertical"
-          className=" justify-center  font-[apple-system,BlinkMacSystemFont] mt-2 w-full"
+          wrapperCol={{ span: 12 }}
+          className=" justify-center  font-[apple-system,BlinkMacSystemFont]  w-max text-center"
         >
-          <h1 className="text-2xl">
-            Welcome Back <br></br>Please Login 😊
-          </h1>
+          <h1 className="text-3xl m-4 ">Welcome Back Please Login 😊</h1>
           <Form.Item
+            className="text-center"
             label="Email"
             name="email"
             rules={[{ required: true, message: "Please Enter Your Email" }]}
           >
-            <input type="email" />
+            <input
+              type="email"
+              placeholder="Enter Your Email"
+              style={{
+                height: "40px",
+                width: "450px",
+                border: "1px solid #8a8a8a",
+                paddingLeft: "15px",
+              }}
+            />
           </Form.Item>
           <Form.Item
             label="Password"
             name="password"
             rules={[{ required: true, message: "Please Enter Your Password" }]}
           >
-            <input type="password" />
+            <input
+              type="password"
+              placeholder="Enter Your Password"
+              style={{
+                height: "40px",
+                width: "450px",
+                border: "1px solid #8a8a8a",
+                paddingLeft: "15px",
+              }}
+            />
           </Form.Item>
-          <Button fullWidth title="Login" type="submit"  />
-          <Divider style={{borderColor:"black"}}>or Lodin with </Divider>
+          <Form.Item wrapperCol={{ offset: 6, span: 10 }}>
+            <Button type="primary" htmlType="submit" block className="border-sky-700 border-solid bg-sky-600">
+              Submit
+            </Button>
+          </Form.Item>
+          <Divider style={{ borderColor: "black" }}>Or Login Using </Divider>
+          <div>
+          <GoogleCircleFilled className="text-2xl text-neutral-950 hover:text-red-600"/>
+          </div>
+          <Link to="/register" className="text-base text-neutral-950 hover:text-red-600" >
+            {" "}
+          Do not have a account? Register</Link>
         </Form>
       </div>
     </div>
---
+++ doctor/client/src/pages/Register/index.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Divider, Form,Button, } from "antd";
+import {GoogleCircleFilled }from "@ant-design/icons"
+import{Link} from "react-router-dom"
+import bgimg from "../../images/Loginbg1.jpg";
+
+
+const Register = () => {
+  return (
+    <div
+      className="flex h-screen bg-cover justify-center items-center "
+      style={{ backgroundImage: `url(${bgimg})` }}
+    >
+      <div className="  flex bg-slate-400/40 w-2/6 justify-center shadow-2xl shadow-slate-200 rounded-md  ">
+        <Form
+          layout="vertical"
+          wrapperCol={{ span: 12 }}
+          className=" justify-center  font-[apple-system,BlinkMacSystemFont]  w-max text-center"
+        >
+          <h1 className="text-3xl m-4 ">Welcome  Please Register 😊</h1>
+          <Form.Item
+            className="text-center"
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please Enter Your Email" }]}
+          >
+            <input
+              type="email"
+              placeholder="Enter Your Email"
+              style={{
+                height: "40px",
+                width: "450px",
+                border: "1px solid #8a8a8a",
+                paddingLeft: "15px",
+              }}
+            />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please Enter Your Password" }]}
+          >
+            <input
+              type="password"
+              placeholder="Enter Your Password"
+              style={{
+                height: "40px",
+                width: "450px",
+                border: "1px solid #8a8a8a",
+                paddingLeft: "15px",
+              }}
+            />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 10 }}>
+            <Button type="primary" htmlType="submit" block className="border-sky-700 border-solid bg-sky-600">
+              Submit
+            </Button>
+          </Form.Item>
+          <Divider style={{ borderColor: "black" }}>Or Login Using </Divider>
+          <div>
+          <GoogleCircleFilled className="text-2xl text-neutral-950 hover:text-red-600"/>
+          </div>
+          <Link to="/login" className="text-base text-neutral-950 hover:text-red-600" >
+            {" "}
+          Do not have a account? Register</Link>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
